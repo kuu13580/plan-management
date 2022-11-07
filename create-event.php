c9e734bcee9e6056536a55d1b134b4887ca07f49
kuu13580 <13580kuu@gmail.com>
2022-11-07 10:51:29 +0900

    イベント作成ページのデザイン変更

--- create-event.php
+++ create-event.php
@@ -9,19 +9,21 @@ include "php/_header.php";
 
 <body>
   <div class="container">
+  <div class="title">イベント新規作成</div>
     <div class="form-wrapper">
       <form action="" method="POST" id="create-form">
-        <div><label class="" for="name">名前</label><input type="text" id="name" required></div>
-        <div><label class="" for="num">人数</label><input type="number" id="num" required></div>
-        <div class="form-check form-switch" style = "visibility: hidden">
-          <label class="form-check-label" for="carUse">車使用</label>
-          <input class="form-check-input" type="checkbox" id="carUse">
+        <div><label class="" for="name">イベント名</label><input type="text" id="name" required></div>
+        <div><label class="" for="num">参加人数</label><input type="number" id="num" required></div>
+        <div class="form-check form-switch">
+          <label class="form-check-label" for="carUse">燃料費概算</label>
+          <input class="form-check-input" type="hidden" id="carUse" value="false">
+          <span style="font-size: 1rem;margin-left: .5rem;color:red">現在この機能は使用できません</span>
         </div>
-        <div class="mb-3 me-3 car-options" style = "visibility: hidden">
+        <div class="mb-3 me-3 car-options">
           <div><label for="kmPl">燃費</label><input type="number" id="kmPl" disabled><span class="unit">km/L</span></label></div>
           <div><label for="costPl">ガソリン価格</label><input type="number" id="costPl" disabled><span class="unit">¥/L</span></div>
         </div>
-        <button type="button" class="btn btn_submit">作成</button>
+        <div class="btn-wrapper"><button type="button" class="btn btn_submit">作成</button></div>
       </form>
     </div>
   </div>
